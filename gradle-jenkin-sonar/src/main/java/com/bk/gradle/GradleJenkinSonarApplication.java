f6e411976ed97f1260d4886af0e0ca5df12f43dc
Biswakalyan Nayak <Biswakalyan.Nayak@solvians.com>
2020-05-19 00:19:18 +0200

    Initial commmit

---
+++ gradle-jenkin-sonar/src/main/java/com/bk/gradle/GradleJenkinSonarApplication.java
@@ -0,0 +1,13 @@
+package com.bk.gradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleJenkinSonarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradleJenkinSonarApplication.class, args);
+	}
+
+}
